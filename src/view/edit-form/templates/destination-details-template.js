df9e32bc0e5404b20dfbd15e39a24218672d2a65
anton-ableton <a.nazarov3@g.nsu.ru>
2026-02-14 18:41:12 +0700

    fix offers view

--- src/view/edit-form/templates/destination-details-template.js
+++ src/view/edit-form/templates/destination-details-template.js
@@ -3,12 +3,22 @@ export function createDestinationDetailsTemplate(destination) {
     return '';
   }
 
-  const { description } = destination;
+  const { description, pictures = [] } = destination;
 
   return `
     <section class='event__section event__section--destination'>
       <h3 class='event__section-title event__section-title--destination'>Destination</h3>
       ${description ? `<p class='event__destination-description'>${description}</p>` : ''}
+      
+      ${pictures.length ? `
+        <div class='event__photos-container'>
+          <div class='event__photos-tape'>
+            ${pictures.map((pic) => `
+              <img class='event__photo' src='${pic.src}' alt='${pic.description}'>
+            `).join('')}
+          </div>
+        </div>
+      ` : ''}
     </section>
   `;
 }
